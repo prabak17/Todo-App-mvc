f7d69774a4623cb58b2621062319c6cda069d4cc
unknown <praab1998@gmail.com>
2022-09-28 18:23:51 +0530

    first commit

---
+++ controllers/todos.js
@@ -0,0 +1,57 @@
+const Todo = require('../models/todo');
+
+module.exports = {
+    getTodos: async(req,res) =>{
+        
+        try{
+            const todoItems = await Todo.find({userId:req.user.id})
+            const itemsLeft = await Todo.countDocuments({userId:req.user.id ,completed: false})
+            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user:req.user})
+        }catch(err){
+            console.log(err)
+        }
+    },
+    createTodo: async(req,res) =>{
+        try{
+            await Todo.create({todo:req.body.todoItem, completed:false, userId: req.user.id})
+            console.log("Todo has been added");
+            res.redirect('/todos')
+        }catch(err){
+            console.log(err);
+        }
+    },
+    deleteTodo: async (req,res) =>{
+        try{
+            await Todo.findOneAndDelete({
+                _id:req.body.todoIdFromJSFile
+            })
+            console.log("Item Deleted");
+            res.json('Deleted It')
+
+        }catch(err){
+            console.log(err);
+        }
+    },
+    markComplete: async(req,res) =>{
+        try{
+            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},
+                {completed:true})
+                console.log('Marked Completed');
+                res.json('Marked Complete')
+        }catch(err){
+            console.log(err)
+        }
+    },
+    markIncomplete: async(req,res)=>{
+        try{
+            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},
+                {completed:false})
+                console.log('Marked Incomplete');
+                res.json('Marked Incomplete')
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+
+}
